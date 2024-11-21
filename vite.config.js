0d0a1ad0c4ecedb917c5b2f9ab49795bc74119f8
Your Name <you@example.com>
2024-11-21 19:52:42 +0000

    publishing3

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,4 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from 'vite'
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        loginSofer: 'driverSignIn.html',
-        incarcare: 'Incarcare.html',
-        descarcare: 'descarcare.html',
-        adminLogin: 'adminLogin.html',
-        contConfirmat: 'contConfirmat.html',
-        creareContCompaniePart1: 'creareContCompanie.html',
-        creareContCompaniePart2: 'creareContCompanie2.html',
-        creareMasina: 'creareMasina.html',
-        cursaFinalizata: 'cursaFinalizata.html',
-        driverSignIn: 'driverSignIn.html',
-        meniuPrincipalCompanie: 'meniuPrincipalCompanie.html',
-        admin: 'paginaAdmin.html'
-      }
-    }
-  }
-});
+base: '',
+})
