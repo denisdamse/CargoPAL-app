398473377b7a272644e344cb0841f33772a72637
Your Name <you@example.com>
2024-11-21 19:36:25 +0000

    publishing3

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        loginSofer: 'driverSignIn.html',
-        incarcare: 'Incarcare.html',
-        descarcare: 'descarcare.html',
-        adminLogin: 'adminLogin.html',
-        contConfirmat: 'contConfirmat.html',
-        creareContCompaniePart1: 'creareContCompanie.html',
-        creareContCompaniePart2: 'creareContCompanie2.html',
-        creareMasina: 'creareMasina.html',
-        cursaFinalizata: 'cursaFinalizata.html',
-        driverSignIn: 'driverSignIn.html',
-        meniuPrincipalCompanie: 'meniuPrincipalCompanie.html',
-        admin: 'paginaAdmin.html'
-      }
-    }
-  }
-});
+  base: 'CargoPAL-app', // Replace with your repository name
+  plugins: [/* your plugins here */],
+})
