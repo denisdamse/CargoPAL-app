6c3dfcab4f72845afe08d8497650f0566c3e64ef
Your Name <you@example.com>
2024-11-22 06:27:23 +0000

    publishing3

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,23 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite";
 export default defineConfig({
-  base: './',  // Ensure relative paths for assets
+  base: "",
   build: {
-    outDir: 'dist',  // Output directory for the build
     rollupOptions: {
-      input: 'index.html',
-      jsfile1: '.pnp.cjs',
-      jsfile2: '.pnp.loader.mjs',
-      accesLocatie: 'accesLocatie.html',
-      adminLogin: 'adminLogin.html',
-      contConfirmat: 'contConfirmat.html',
-      jsfile3: 'counter.js',
-      creareContCompanie: 'creareContCompanie.html',
-      creareContCompanie2: 'creareContCompanie2.html',
-      creareMasina: 'creareMasina.html',
-      cursaFinalizata: 'cursaFinalizata.html',
-      descare: 'descarcare.html',
-      driver: 'driverSignIn.html',
-      incarcare:'Incarcare.html',
-      main: 'main.js',
-      meniuPrincipal: 'meniuPrincipalCompanie.html',
-      pac: 'package.json',
-      paginaAdmin: 'paginaAdmin.html',
-      config: 'postcss.config.js',
-      style: 'style.css',
-      styleconfig: 'tailwind.config.js',
-      viteconfig: 'vite.config.js',
-      yarnfile: 'yarn.lock'
-    },
-  },
-  publicDir: 'CargoPAL-app',  // Static assets will be taken from here
+      input: ["index.html", 
+        "./acceslocatie.html",
+        "adminLogin.html",
+        "asteptareConfirmareContCompanie.html",
+        "contConfirmat.html",
+        "creareContCompanie.html",
+        "creareContCompanie2.html",
+        "creareMasina.html",
+        "cursaFinalizata.html",
+        "descarcare.html",
+        "driverSignIn.html",
+        "Incarcare.html",
+        "meniuPrincipalCompanie.html",
+        "paginaAdmin.html",
+      ],
+    },
+  },
 });
